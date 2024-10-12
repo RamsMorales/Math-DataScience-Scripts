82d2b6d6b729fbc6491f4e7243f2947aebe2c350
Ramson Munoz <rmuno@protonmail.com>
2024-10-12 13:08:23 -0400

    added try catch to check for valid matrix input for exercise 1

--- QR_Factorization.py
+++ QR_Factorization.py
@@ -5,9 +5,32 @@ Created on Sat Oct 12 12:20:26 2024
 
 @author: ramsonmunoz
 """
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import sys
+
+
+def exercise1(matrix: np.ndarray):
+    
+    try:
+        
+        if(len(matrix.shape) != 2):
+            raise Exception("Invalid dimension. Expected dim 2 but got %d" %(len(matrix.shape)))
+        elif(matrix.shape[0] != matrix.shape[1]):
+            raise Exception("Please ensure a square matrix. Current matrix is %d by %d" %(matrix.shape[0],matrix.shape[1]))
+        
+    except Exception as e:
+        print(e)
+        sys.exit()
+    
+    
+
 
 def main():
-    print("hello world")
+    
+    testTensor = np.random.rand(2,2,2)
+    exercise1(testTensor)
     
 if __name__=="__main__":
     main()
