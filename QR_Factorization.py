d8285998dc5be25ebc07c095905005a52f277b9d
Ramson Munoz <rmuno@protonmail.com>
2024-10-12 14:37:18 -0400

    added QR factorization algorithm and method to solve a system using QR method

--- QR_Factorization.py
+++ QR_Factorization.py
@@ -10,14 +10,34 @@ import scipy as sp
 import matplotlib.pyplot as plt
 import sys
 
+def factorizationQR(matrix: np.ndarray) -> np.ndarray:
+    
+    numVectors= matrix.shape[0]
+    dimensions = matrix.shape[1]
+    
+    L = np.zeros(numVectors)
+    for i in range(numVectors):
+        L[i] = np.sqrt(matrix[i].T @ matrix[i])
+    
+    V = matrix.copy() / L
+    Q = V.copy()
+    
+    for j in range(0,numVectors):
+        Q[j] = V[j]/np.sqrt(V[j].T @ V[j])
+        for k in range(j, numVectors):
+            V[k] = V[k] - (Q[j].T @ V[k] * Q[j])
+            
+    R = Q.T @ matrix
+            
+    return Q, R
 
-def exercise1(matrix: np.ndarray):
+def solveLinearSystemQR(matrix: np.ndarray, vector: np.ndarray) -> np.ndarray:
     
     try:
         
         #This block is used to verify the inputs from the problem statement. end will be signified by ****** 
         
-        '''Here we ensure a square n x n matrix that is invertible'''
+        '''Here we ensure a square n x n matrix that is invertible and our solution vector is of dimension n'''
         
         #*****************************************************************************************************************
         if(len(matrix.shape) != 2):
@@ -30,7 +50,28 @@ def exercise1(matrix: np.ndarray):
         
         if np.isclose(det_matrix, 0):
             raise Exception("Matrix is not invertible. Please ensure invertible matrix")
+            
+        if (vector.shape[0] != matrix.shape[1]):
+            raise Exception("Vector has invalid dimension. Expeced %d but got %d" %(matrix.shape[1], vector.shape[0]))
         #*****************************************************************************************************************
+        
+        '''
+        Solving the system of equations with QR factorization. 
+        '''
+        Q,R = factorizationQR(matrix)
+        
+        transposeQ = Q.T
+        
+        y = transposeQ @ vector # y = Q.T @ b
+        
+        solution = np.linalg.inv(R) @ y # back substitution to find x
+        
+        
+        return solution
+        
+        
+        
+        
         
         
     except Exception as e:
@@ -41,12 +82,13 @@ def exercise1(matrix: np.ndarray):
 
 
 def main():
+    #test system with known solution x = [1 -1].T
+    A = np.array([[1,1],[2,1]])
     
-    #testTensor = np.random.rand(2,2,2)
-    testNonSquare = np.random.rand(3,2)
-    print(testNonSquare.shape)
-    testNonInvertible = np.array([[0,0],[0,1]])
-    exercise1(testNonSquare)
+    b = np.array([[0],[1]])
+    
+    
+    print(solveLinearSystemQR(A,b)) # it worked
     
 if __name__=="__main__":
     main()
