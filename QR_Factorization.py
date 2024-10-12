9b8eb8f340c346e5d929bac0ba920523ed55ff25
Ramson Munoz <rmuno@protonmail.com>
2024-10-12 13:19:09 -0400

    ensured square invertible matrix as input for exercise 1

--- QR_Factorization.py
+++ QR_Factorization.py
@@ -15,10 +15,23 @@ def exercise1(matrix: np.ndarray):
     
     try:
         
+        #This block is used to verify the inputs from the problem statement. end will be signified by ****** 
+        
+        '''Here we ensure a square n x n matrix that is invertible'''
+        
+        #*****************************************************************************************************************
         if(len(matrix.shape) != 2):
             raise Exception("Invalid dimension. Expected dim 2 but got %d" %(len(matrix.shape)))
-        elif(matrix.shape[0] != matrix.shape[1]):
+            
+        if(matrix.shape[0] != matrix.shape[1]):
             raise Exception("Please ensure a square matrix. Current matrix is %d by %d" %(matrix.shape[0],matrix.shape[1]))
+            
+        det_matrix = np.linalg.det(matrix)
+        
+        if np.isclose(det_matrix, 0):
+            raise Exception("Matrix is not invertible. Please ensure invertible matrix")
+        #*****************************************************************************************************************
+        
         
     except Exception as e:
         print(e)
@@ -29,8 +42,11 @@ def exercise1(matrix: np.ndarray):
 
 def main():
     
-    testTensor = np.random.rand(2,2,2)
-    exercise1(testTensor)
+    #testTensor = np.random.rand(2,2,2)
+    testNonSquare = np.random.rand(3,2)
+    print(testNonSquare.shape)
+    testNonInvertible = np.array([[0,0],[0,1]])
+    exercise1(testNonSquare)
     
 if __name__=="__main__":
     main()
